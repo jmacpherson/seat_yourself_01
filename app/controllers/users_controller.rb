b59d08563d19b241ca6f84841e004c84fc0015db
michaelsharpe <m.sharpe@project89.org>
2013-11-16 22:29:51 -0500

    Added new user form and create method.  Sorcery encryption works.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,12 @@ class UsersController < ApplicationController
   end
   
   def create
-    
+    @user = User.new user_params
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
   
   
