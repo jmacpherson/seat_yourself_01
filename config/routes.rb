6c7ba64953496344ba6c90cdaf52fe6a8097b396
michaelsharpe <m.sharpe@project89.org>
2013-11-17 13:37:41 -0500

    Sessions added and functional.  Log in and log out.  Nav bar added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SorceryPostgresql::Application.routes.draw do
   root to: "users#index"
   resources :users
+  resources :user_sessions, :only => [:new, :create, :destroy]
+
+  get "login" => "user_sessions#new", :as => :login
+  get "logout" => "user_sessions#destroy", :as => :logout
 end
